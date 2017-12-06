1599e8e3de38e3f9a269eb4336328480874c13b3
Chinseok Lee <allieuslee@gmail.com>
2017-12-06 10:25:34 +0900

    멜론 검색 기능 추가

--- plusfriend/urls.py
+++ plusfriend/urls.py
@@ -1,6 +1,11 @@
-
-from django.urls import path
+from django.urls import re_path
+from . import views
 
 urlpatterns = [
+    re_path(r'^keyboard$', views.on_init),
+    re_path(r'^friend$', views.on_added),
+    re_path(r'^friend/(?P<user_key>[\w-]+)$', views.on_block),
+    re_path(r'^chat_room/(?P<user_key>[\w-]+)$', views.on_leave),
+    re_path(r'^message$', views.on_message),
 ]
 
